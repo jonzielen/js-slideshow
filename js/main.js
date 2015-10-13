bdf608ff57fcf72c1de34e09d7c3e6999f892639
Jon Zielenkievicz <jonzielen@gmail.com>
2015-10-13 00:25:58 -0400

    adds working prototype

--- js/main.js
+++ js/main.js
@@ -19,10 +19,29 @@ function addNavButtons(lowerNavList) {
     for (var i = 0; i < lowerNavList; i++) {
         var li = document.createElement('li');
         li.innerHTML = i + 1;
+        li.setAttribute('data-index', i + 1);
         if (i == 0) {
             li.setAttribute('id', 'selected');
         }
 
         navList.appendChild(li);
+    }
+}
+
+// slideshow
+var fullNavList = document.querySelector('#nav-list').childNodes;
+
+for (var i = 0; i < fullNavList.length; i++) {
+    if (fullNavList[i].tagName == 'LI') {
+        fullNavList[i].addEventListener('click', function() {
+            document.getElementById('selected').setAttribute('id', '');
+            this.setAttribute('id', 'selected');
+
+            var newActiveIndex = this.getAttribute('data-index');
+
+            document.getElementById('active').setAttribute('id', '');
+            document.getElementsByClassName('slideshow')[newActiveIndex-1].setAttribute('id', 'active');
+
+        }, false);
     }
 }
